f6d9751d0d4bf107262ef92675436d0d641e31a1
DinhKhang2002 <khang.dv204568@sis.hust.edu.vn>
2025-12-24 20:27:11 +0700

    Update types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -9,9 +9,26 @@ declare module 'qrcode'
 
 interface ImportMetaEnv {
   readonly VITE_API_BASE?: string
+  readonly PROD: boolean
+  readonly DEV: boolean
+  readonly SSR: boolean
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
+  readonly url: string
+  readonly hot?: ViteHotContext
+  readonly glob: any
+}
+
+interface ViteHotContext {
+  accept(): void
+  accept(callback: (module: any) => void): void
+  dispose(callback: (data: any) => void): void
+  decline(): void
+  invalidate(message?: string): void
+  on(event: string, cb: (...args: any[]) => void): void
+  off(event: string, cb: (...args: any[]) => void): void
+  send(event: string, data: any): void
 }
 
